b9334ecca5131bc38b18dcd1c3e6ce041345371c
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-11 10:37:43 +0530

    server- app.js- Express object required and setup completd.

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("Hello wo!");
+});
+
+app.listen(8080, () => {
+  console.log("Connected");
+});
