695b42666636d9a7875d7d57e30fcfbd696ec0ff
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-11 10:50:03 +0530

    server- app.js- Basic setup completed.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+//Dot env config
+dotenv.config();
+
+//Rest Obj
 const app = express();
 
+//Middleware
+app.use(morgan("dev")); //Call morgan showing in console
+
+//Routes
 app.use("/", (req, res, next) => {
   res.send("Hello wo!");
 });
 
-app.listen(8080, () => {
-  console.log("Connected");
+//Port
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} mode on port ${PORT}`
+      .bgMagenta.white
+  );
 });
