227732b2405cb4c5d341be883a0b9a4c8847a917
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-11 11:57:24 +0530

    server- models- UserModel.js- Schema designed.

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -5,5 +5,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
 });
