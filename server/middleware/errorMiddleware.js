83ba05f063fce6dd3978022dc31f481acbfb0326
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-15 12:11:43 +0530

    server- middleware- errorMiddleware.js- Handled custom middleware code fixed.

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,2 +1,10 @@
 //******* ERROR MIDDLEWARE || NEXT Function *********/
-const errorMiddleware = () => {};
+const errorMiddleware = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({
+    message: "Something went wrong!",
+    err,
+  });
+};
+
+export default errorMiddleware;
