d6cb9e53785d8d5ff6f406fb7fdac4736602add4
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-15 12:31:44 +0530

    server- middleware- errorMiddleware.js- Required field error code fixed.

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -3,15 +3,26 @@ const errorMiddleware = (err, req, res, next) => {
   console.log(err);
   const defaultErrors = {
     statusCode: 500,
-    message: "Something went wrong!",
+    message: err,
   };
-  res.status(500).send({
-    message: "Something went wrong!",
-    err,
-  });
+
   //required field error
   if (err.name === "ValidationError") {
+    defaultErrors.statusCode = 422;
+    defaultErrors.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
   }
+  //Dulicate error
+  if(err.code && err.code===11000){
+    defaultErrors.statusCode=400;
+
+  }
+
+
+  res.status(defaultErrors.statusCode).json({
+    message: defaultErrors.message,
+  });
 };
 
 export default errorMiddleware;
