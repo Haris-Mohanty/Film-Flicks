e6b16b1fcbfcc1097ac06c683a4f68882a8a13a8
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-17 10:52:59 +0530

    server- controller- userController.js(Login)- Validation added.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -138,6 +138,23 @@ export const deleteUser = async (req, res, next) => {
 //************* LOGIN USER ************/
 export const login = async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+    
+    //Validation
+    if (!name) {
+      next("Please provide all fields!");
+    }
+    if (!email) {
+      next("Please provide all fields!");
+    }
+    if (!password) {
+      next("Please provide all fields!");
+    }
+    if (password.length < 6) {
+      next("Password length should be greater than 6 character!");
+    }
+
+    
   } catch (error) {
     next(error);
   }
