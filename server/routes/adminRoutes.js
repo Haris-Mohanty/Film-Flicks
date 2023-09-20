a8d999a351b876680ea4e14f8993d8613dc5d81e
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-20 20:51:36 +0530

    server- routes- adminRoutes.js- Routes for admin signup code processing...

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { signupAdmin } from "../controllers/adminController.js";
 
 //Router Object
 const router = express.Router();
 
 //Signup Admin
-router.post("/signup");
+router.post("/signup", signupAdmin);
+
+
+//
