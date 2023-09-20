09825aaaa3bd77ba7fb85a8c4695e19c26fa9f19
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-20 20:39:44 +0530

    server- controller- adminController.js- Validation code processing....

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,7 +1,21 @@
+//********** SIGNUP ADMIN *************/
 export const signupAdmin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    
+
+    //Validation
+    if (!name) {
+      next("Please provide all fields!");
+    }
+    if (!email) {
+      next("Please provide all fields!");
+    }
+    if (!password) {
+      next("Please provide all fields!");
+    }
+    if (password.length < 6) {
+      next("Password length should be greater than 6 character!");
+    }
   } catch (error) {
     next(error);
   }
