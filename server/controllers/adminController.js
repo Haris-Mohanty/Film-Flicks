fdb8b64ab7a1ce9eacc8a7c9f268fc1c4c99fb1e
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-20 20:42:12 +0530

    server- controller- adminController.js- (signup admin)- Check existing admin code processing......

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -4,13 +4,15 @@ export const signupAdmin = async (req, res, next) => {
     const { email, password } = req.body;
 
     //Validation
-
     if (!email || !password) {
       next("Please provide all fields!");
     }
     if (password.length < 6) {
       next("Password length should be greater than 6 character!");
     }
+
+    //Check Admin(Already exists or not)
+    const existingAdmin=
   } catch (error) {
     next(error);
   }
