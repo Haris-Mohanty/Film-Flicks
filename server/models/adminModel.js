2126597441cb722eee3a89266102a8ae3c3993cd
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-20 20:25:19 +0530

    server- model- adminModel.js- Admin schema created successfully.

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
-const adminSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        unique:true
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
     },
-    
-});
+    password: {
+      type: String,
+      minLength: [6, "Password length should be greater than 6 character!"],
+    },
+    addedMovies: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
