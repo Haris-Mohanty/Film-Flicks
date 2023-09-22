fa7915e737e80ed2e7bfce0779bcc63365807f99
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-22 16:57:50 +0530

    server- routes- movieRoutes.js- Add movies routes created successfully.

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,12 +1,15 @@
 //************* ADD MOVIES  ************/
 export const addMovies = async (req, res, next) => {
   try {
+    //Get token and verify
     const token = req.headers.authorization;
     if (!token && token.trim() === "") {
       return res.status(404).send({
         message: "Authorization Failed!",
       });
     }
+    console.log(token);
+    //
   } catch (err) {
     console.log(err);
   }
--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-
+import { addMovies } from "../controllers/movieController";
 
 //Router Object
 const router = express.Router();
 
-router.post("/", )
+//Add  movies
+router.post("/", addMovies);
+
+//Export router
+export default router;
