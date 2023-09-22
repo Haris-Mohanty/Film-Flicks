9f01f4f3c0c57a31d8dd2c19ac5f5495381907fb
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-22 16:56:12 +0530

    server- controller- movieController.js- Token verified.

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,2 +1,13 @@
 //************* ADD MOVIES  ************/
-export const addMovies = async (req, res, next) => {};
+export const addMovies = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token && token.trim() === "") {
+      return res.status(404).send({
+        message: "Authorization Failed!",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
