2fca1f18693bcf72eb300fff9e1cfebd2db296b0
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-22 14:22:56 +0530

    server- models- movieModel.js- Movie schema created successfully.

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,3 +1,32 @@
 import mongoose from "mongoose";
 
-const movieSchema = new mongoose.Schema({});
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Movie title is Required!"],
+  },
+  description: {
+    type: String,
+    required: [true, "Movie description is Required!"],
+  },
+  releaseDate: {
+    type: Date,
+    required: [true, "Movie release date is Required!"],
+  },
+  posterUrl: {
+    type: String,
+    required: [true, "Movie poster url is Required!"],
+  },
+  featured: {
+    type: Boolean,
+  },
+  bookings: [
+    {
+      type: String,
+    },
+  ],
+  admin: {
+    type: String,
+    required: [true, "Admin Name is Required!"],
+  },
+});
