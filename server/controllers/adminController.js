36659bf7b95a5b0133280028e7b848b4d6f50531
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-25 23:09:49 +0530

    server- models- bookingModel.js- Create a schema for movie bookings

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -93,8 +93,5 @@ export const loginAdmin = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
-    return res.status(500).send({
-      message: "Error in admin login API!",
-    });
   }
 };
--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -116,3 +116,5 @@ export const getMoviesById = async (req, res, next) => {
     });
   }
 };
+
+
