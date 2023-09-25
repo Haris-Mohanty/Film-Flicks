c452c2455efc2d381ca62301774c66e5ac8fb410
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-25 23:19:58 +0530

    server- routes- bookingRoutes.js- Routes created for booking new movies.

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { newBookings } from "../controllers/bookingController.js";
 
 //Router Object
 const router = express.Router();
 
-//
+//Movie Booking (New Bookings)
+router.post("/", newBookings);
 
 //Export Router Obj
 export default router;
