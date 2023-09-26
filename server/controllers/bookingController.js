358562044d03971e93c984afb5d000950e8d7c18
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-26 12:57:44 +0530

    server- controller- bookingController.js(new bookings)- Controller function code processing......

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,10 @@
 export const newBookings = async (req, res, next) => {
-    
+  try {
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: "Error in movie booking API!",
+      err,
+    });
+  }
 };
