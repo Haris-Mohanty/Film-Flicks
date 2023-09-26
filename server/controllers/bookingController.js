a56610bf0b58c885c6b70c1b30d18219ff16a375
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-26 13:02:58 +0530

    server- controller- bookingController.js(new bookings)- Controller function code fixed.

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,22 @@
+import bookingModel from "../models/bookingModel";
+
 export const newBookings = async (req, res, next) => {
   try {
+    const { movie, date, seatNumber, user } = req.body;
+
+    //New booking
+    let bookings = new bookingModel({
+      movie,
+      date: new Date(`${date}`),
+      seatNumber,
+      user,
+    });
+
+    await bookings.save();
+    res.status(201).send({
+      message: "Booking a new Movie Successfully!",
+      bookings,
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).send({
--- server/models/bookingModel.js
+++ server/models/bookingModel.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
-const bookingSchema = new mongoose.Schema({
-  movie: {
-    type: String,
-    required: true,
+const bookingSchema = new mongoose.Schema(
+  {
+    movie: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    seatNumber: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: String,
+      required: true,
+    },
   },
-  date: {
-    type: Date,
-    required: true,
-  },
-  seatNumber: {
-    type: Number,
-    required: true,
-  },
-  user: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 //Export
 export default mongoose.model("Booking", bookingSchema);
