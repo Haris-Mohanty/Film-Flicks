a4a4b554415ced601e85f84f7d4c8bb50110354c
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-29 12:59:42 +0530

    client- src- components- Header.jsx- Add all elements in header component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      
+      <Header/>
     </>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  AppBar,
+  Autocomplete,
+  Box,
+  Tab,
+  Tabs,
+  TextField,
+  Toolbar,
+} from "@mui/material";
+import VideoSettingsIcon from "@mui/icons-material/VideoSettings";
+
+const movieArray = ["Khsi", "Jawan", "KGF"];
 
 const Header = () => {
+  const [value, setValue] = useState(0);
+
   return (
     <>
-      
+      <AppBar sx={{ bgcolor: "#2b2d42" }}>
+        <Toolbar>
+          <Box width={"20%"}>
+            <VideoSettingsIcon />
+          </Box>
+          <Box width={"35%"} margin={"auto"}>
+            <Autocomplete
+              freeSolo
+              options={movieArray.map((option) => option)}
+              renderInput={(params) => (
+                <TextField
+                  sx={{ input: { color: "white" } }}
+                  variant="standard"
+                  {...params}
+                  placeholder="Search Movies"
+                />
+              )}
+            />
+          </Box>
+          <Box display={"flex"}>
+            <Tabs
+              textColor="inherit"
+              indicatorColor="secondary"
+              value={value}
+              onChange={(e, value) => setValue(value)}
+            >
+              <Tab label="Admin" />
+              <Tab label="Auth" />
+              <Tab label="Movie" />
+            </Tabs>
+          </Box>
+        </Toolbar>
+      </AppBar>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
