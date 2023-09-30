ab7bdb6cb63450f94adc682e0a4d6832ad4ead1f
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-30 20:51:07 +0530

    client- src- api- api.js- Get all movies from databse code fixed.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,2 +1,14 @@
-import axios from 'axios'
-export const getAllMovies = async () => {};
+import axios from "axios";
+export const getAllMovies = async () => {
+  const response = await axios
+    .get("http://localhost:8080/api/v1/movie")
+    .catch((err) => console.log(err));
+
+  //Validation
+  if (response.status !== 200) {
+    return console.log("No Data");
+  }
+
+  const data = await response.data;
+  return data;
+};
