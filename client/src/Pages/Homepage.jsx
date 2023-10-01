752abc04848ede57cdeb4989b8ed8927ed82b504
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-01 22:48:20 +0530

    client- src- Pages- Homepage.jsx- Add banner and latest movie showing in cards code processing.......

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -1,10 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Homepage = () => {
   return (
     <>
-      <Box width={"100vw"} height={"100vh"} margin={"auto"} marginTop={2}></Box>
+      <Box width={"100%"} height={"100%"} margin={"auto"} marginTop={2}>
+        <Box margin={"auto"} width={"90%"} height={"50vh"} padding={2}>
+          <img
+            src="https://static.moviecrow.com/gallery/20230508/215637-Jawan%20Release%20Date%20Shah%20Rukh%20Khan%20Poster.png"
+            alt="movies1"
+            width={"100%"}
+            height={"100%"}
+          />
+        </Box>
+        <Box padding={5} margin={"auto"}>
+          <Typography variant="h4" textAlign={"center"} fontWeight={"bold"}>
+            Latest Release
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 };
