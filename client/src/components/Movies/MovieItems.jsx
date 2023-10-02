604f0eeaf44afce51ae3fd949c5eeb70a62eaae8
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-02 22:05:42 +0530

    client- src- components- Movies- MovieItems.jsx- Card create successfully.

--- client/src/components/Movies/MovieItems.jsx
+++ client/src/components/Movies/MovieItems.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import React from "react";
 
 const MovieItems = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default MovieItems
+export default MovieItems;
