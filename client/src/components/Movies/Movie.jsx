55462b86b75c8c7b0b516d7529381eff17080714
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-03 12:42:29 +0530

    client- src- Pages- Movies- Movie.js- Show all movies code fixed.

--- client/src/components/Movies/Movie.jsx
+++ client/src/components/Movies/Movie.jsx
@@ -1,12 +1,51 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { getAllMovies } from "../../api/api";
+import MovieItems from "./MovieItems";
 
 const Movie = () => {
+  const [movies, setMovies] = useState([]);
+  //Show all Movies
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => setMovies(data.movies))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Box margin={"auto"} marginTop={4}>
-      <Typography variant='h4'></Typography>
+      <Typography
+        margin={"auto"}
+        variant="h4"
+        padding={2}
+        width={"40%"}
+        bgcolor={"#783eb3"}
+        color={"#fff"}
+        fontWeight={"bold"}
+        textAlign={"center"}
+      >
+        All Movies
+      </Typography>
+      <Box
+        width={"100%"}
+        margin={"auto"}
+        display={"flex"}
+        justifyContent={"center"}
+        flexWrap={"wrap"}
+      >
+        {movies &&
+          movies.map((movie, index) => (
+            <MovieItems
+              key={index}
+              id={movie.id}
+              posterUrl={movie.posterUrl}
+              releaseDate={movie.releaseDate}
+              title={movie.title}
+            />
+          ))}
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;
