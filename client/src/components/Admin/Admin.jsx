5c1c80473e01c062d14b62f1576f86d0b24c16ea
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-08 13:10:55 +0530

    client- src- components- Admin- Admin.jsx- Admin authentication code fixed.

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -4,7 +4,9 @@ import { sendAdminLoginReq } from "../../api/api";
 
 const Admin = () => {
   const getData = (data) => {
-    sendAdminLoginReq(data.inputs).then((res)=>{console.log(res)})
+    sendAdminLoginReq(data.inputs)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
   return (
     <>
