fb208561ce97c28a9296ad5c144eca10bbebbeb5
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-09 20:45:39 +0530

    client- index.js- Provide added.

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import axios from "axios";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
