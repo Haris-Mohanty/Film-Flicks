5bb14edf0ade107dd65404f8f241ccfc45d64e62
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-13 14:04:18 +0530

    client/src/components/Bookings/Bookings.jsx- Get movie details when click on book code fixed.

--- client/src/components/Bookings/Booking.jsx
+++ client/src/components/Bookings/Booking.jsx
@@ -3,13 +3,16 @@ import { useParams } from "react-router-dom";
 import { getMovieDetails } from "../../api/api";
 
 const Booking = () => {
-  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
 
   const id = useParams().id;
 
   useEffect(() => {
-    getMovieDetails(id);
+    getMovieDetails(id)
+      .then((res) => setMovie(res.movie))
+      .catch((err) => console.log(err));
   }, [id]); //When the id is change, call the useEffect once again
+  
   return <div></div>;
 };
 
