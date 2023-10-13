4b519993fe91e2d9fdfbec8634ddd7cc45f0d14c
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-13 14:07:29 +0530

    client/src/components/Bookings/Bookings.jsx- Booking movie ui design processing.....

--- client/src/components/Bookings/Booking.jsx
+++ client/src/components/Bookings/Booking.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieDetails } from "../../api/api";
+import { Typography } from "@mui/material";
 
 const Booking = () => {
   const [movie, setMovie] = useState();
@@ -12,8 +13,23 @@ const Booking = () => {
       .then((res) => setMovie(res.movie))
       .catch((err) => console.log(err));
   }, [id]); //When the id is change, call the useEffect once again
-  
-  return <div></div>;
+
+  return (
+    <div>
+      {movie && (
+        <Fragment>
+          <Typography
+            padding={3}
+            fontFamily={"fantasy"}
+            variant="h4"
+            textAlign={"center"}
+          >
+            Book tickets of the Movie: {movie.title}
+          </Typography>
+        </Fragment>
+      )}
+    </div>
+  );
 };
 
 export default Booking;
