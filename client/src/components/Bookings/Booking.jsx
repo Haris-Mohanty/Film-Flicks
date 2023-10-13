dd283551d7f5741da6e616e1607718608bba5df1
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-13 12:48:32 +0530

    client/src/components/Bookings/Bookings.jsx- Call the getMovieDetails function by using useEffect.

--- client/src/components/Bookings/Booking.jsx
+++ client/src/components/Bookings/Booking.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getMovieDetails } from "../../api/api";
 
 const Booking = () => {
+  const [movies, setMovies] = useState();
+
   const id = useParams().id;
-  console.log(id)
+
+  useEffect(() => {
+    getMovieDetails(id);
+  }, [id]); //When the id is change, call the useEffect once again
   return <div></div>;
 };
 
