9228e007e5df9cc4ffaec35ae7b819a73812ca64
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-13 12:43:14 +0530

    client/src/app.js- Routes added for Bookings.

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Auth from "./components/Auth/Auth";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { adminActions, userActions } from "./store";
+import Booking from "./components/Bookings/Booking";
 
 function App() {
   const dispatch = useDispatch();
@@ -32,6 +33,7 @@ function App() {
           <Route path="/movies" element={<Movie />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/booking/:id" element={<Booking />} />
         </Routes>
       </section>
     </>
