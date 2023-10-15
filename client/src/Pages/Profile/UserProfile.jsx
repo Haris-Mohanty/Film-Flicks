bed9366c0ce7cfb3f78e12fbfcb04fef6a04b350
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-15 07:03:58 +0530

    client/src/Pages/Profile/UserProfile.jsx- Get all booking of user by calling api code fixed.

--- client/src/Pages/Profile/UserProfile.jsx
+++ client/src/Pages/Profile/UserProfile.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react'
-import { getUserBookings } from '../../api/api'
+import React, { useEffect, useState } from "react";
+import { getUserBookings } from "../../api/api";
 
 const UserProfile = () => {
+  const [bookings, setBookings] = useState();
 
-    //*********** GET ALL BOOKING OF USER ***********/
-    useEffect(()=>{
-        getUserBookings
-    },[])
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  //*********** GET ALL BOOKING OF USER ***********/
+  useEffect(() => {
+    getUserBookings()
+      .then((res) => setBookings(res.bookings))
+      .catch((err) => console.log(err));
+  }, []);
+  
+  return <div>
+    
+  </div>;
+};
 
-export default UserProfile
+export default UserProfile;
