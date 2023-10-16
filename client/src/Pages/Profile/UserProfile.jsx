41d5b451d7df5a5afcd95d78ab0850e47d8fbd91
Haris Mohanty <harismohanty8658@gmail.com>
2023-10-16 21:45:37 +0530

    client/src/Pages/Profile/UserProfile.jsx- Showing booking code processing....

--- client/src/Pages/Profile/UserProfile.jsx
+++ client/src/Pages/Profile/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getUserBookings } from "../../api/api";
 import { Box, Typography } from "@mui/material";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const UserProfile = () => {
   const [bookings, setBookings] = useState();
@@ -15,9 +15,22 @@ const UserProfile = () => {
 
   return (
     <Box width={"100%"} display={"flex"}>
-      <Box flexDirection={"column"} justifyContent={"center"} alignItems={"center"} width={"30%"}>
-        <AccountCircleIcon sx={{fontSize:"10rem"}}/>
-        <Typography padding={}></Typography>
+      <Box
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"30%"}
+      >
+        <AccountCircleIcon sx={{ fontSize: "10rem" }} />
+        <Typography
+          padding={1}
+          width={"auto"}
+          textAlign={"center"}
+          border={"1px solid #ccc"}
+          borderRadius={6}
+        >
+          
+        </Typography>
       </Box>
       <Box width={"70%"}></Box>
     </Box>
