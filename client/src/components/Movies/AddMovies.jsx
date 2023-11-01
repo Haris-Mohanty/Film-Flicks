24616b3fd01a19464dd486c0617b41425b881bda
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-01 12:04:21 +0530

    client/src/components/Movies/AddMovies.jsx- Form created/

--- client/src/components/Movies/AddMovies.jsx
+++ client/src/components/Movies/AddMovies.jsx
@@ -1,4 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const AddMovies = () => {
@@ -13,7 +20,49 @@ const AddMovies = () => {
           flexDirection={"column"}
           boxShadow={"10px 10px 20px #ccc"}
         >
-            <Typography textAlign={"center"}></Typography>
+          <Typography textAlign={"center"} variant="h5" fontFamily={"verdana"}>
+            Add New Movie
+          </Typography>
+          <FormLabel>Title</FormLabel>
+          <TextField
+            name="title"
+            variant="standard"
+            margin="normal"
+            sx={{ marginBottom: "17px" }}
+          />
+          <FormLabel>Description</FormLabel>
+          <TextField
+            name="description"
+            variant="standard"
+            margin="normal"
+            sx={{ marginBottom: "17px" }}
+          />
+          <FormLabel>Poster Url</FormLabel>
+          <TextField
+            name="posterUrl"
+            variant="standard"
+            margin="normal"
+            sx={{ marginBottom: "17px" }}
+          />
+          <FormLabel>Release Date</FormLabel>
+          <TextField
+            name="releaseDate"
+            variant="standard"
+            margin="normal"
+            sx={{ marginBottom: "17px" }}
+          />
+          <FormLabel>Actor</FormLabel>
+          <Box display={"flex"}>
+            <TextField
+              name="actor"
+              variant="standard"
+              margin="normal"
+              sx={{ marginBottom: "17px" }}
+            />
+            <Button>Add</Button>
+          </Box>
+          <FormLabel>Featured</FormLabel>
+          <Checkbox checked={false} />
         </Box>
       </form>
     </>
