c89446d28da40d17c36a106459612f71637b36f0
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-06 12:23:55 +0530

    client/src/Pages/Profile/userProfile.jsx- Show user profile details and bookings code processing....

--- client/src/Pages/Profile/UserProfile.jsx
+++ client/src/Pages/Profile/UserProfile.jsx
@@ -12,79 +12,20 @@ const UserProfile = () => {
       .then((res) => setBookings(res.bookings))
       .catch((err) => console.log(err));
   }, []);
-  console.log(bookings)
+  console.log(bookings);
 
   return (
     <Box width={"100%"} display={"flex"}>
-      {bookings && bookings.length > 0 && (
-        <Fragment>
-          {" "}
-          <Box
-            flexDirection={"column"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            width={"30%"}
-            padding={3}
-          >
-            <AccountCircleIcon
-              sx={{ fontSize: "10rem", textAlign: "center", ml: 10 }}
-            />
-            <Typography
-              padding={1}
-              width={"70%"}
-              textAlign={"center"}
-              border={"1px solid #ccc"}
-              borderRadius={6}
-            >
-              {bookings[2] && `Name: ${bookings[2].name}`}
-            </Typography>
-            <Typography
-              mt={1}
-              padding={1}
-              width={"70%"}
-              textAlign={"center"}
-              border={"1px solid #ccc"}
-              borderRadius={6}
-            >
-              {bookings[2] && `Email: ${bookings[2].email}`}
-            </Typography>
-          </Box>
-          <Box width={"70%"} display={"flex"} flexDirection={"column"}>
-            <Typography
-              variant="h3"
-              fontWeight={"bold"}
-              textAlign={"center"}
-              padding={2}
-            >
-              Bookings
-            </Typography>
-            <Box
-              margin={"auto"}
-              display={"flex"}
-              flexDirection={"column"}
-              width={"80%"}
-            >
-              <List>
-                {bookings.map((booking, index) => (
-                  <ListItem
-                    key={index}
-                    sx={{
-                      textAlign: "center",
-                      margin: 2,
-                    }}
-                  >
-                    <ListItemText
-                      sx={{ margin: 1, width: "auto", textAlign: "left" }}
-                    >
-                      {booking.title && `Movie: ${booking.title}`}
-                    </ListItemText>
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-          </Box>
-        </Fragment>
-      )}
+      <Box
+        width={"30%"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        
+      </Box>
+
+      <Box width={"70%"}></Box>
     </Box>
   );
 };
